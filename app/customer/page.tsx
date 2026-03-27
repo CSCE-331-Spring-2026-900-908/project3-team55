8d866f42c0f2d813cea515377481afb295bf590b
sean-m32 <77125011+sean-m32@users.noreply.github.com>
2026-03-27 10:50:27 -0500

    created placeholder pages for the 4 main interfaces

---
+++ app/customer/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Placeholder for Customer Kiosk Menu</h1>
+  );
+}
---
+++ app/manager/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <h1>Placeholder for Manager Menu</h1>
+  );
+}
